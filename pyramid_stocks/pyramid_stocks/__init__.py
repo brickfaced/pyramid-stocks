361f0f5c13c1879880b11bbf5329b26e11e0d8a9
brickfaced <brickfazed@hotmail.com>
2018-04-15 17:58:59 -0700

    fixed init

--- pyramid_stocks/pyramid_stocks/__init__.py
+++ pyramid_stocks/pyramid_stocks/__init__.py
@@ -6,7 +6,7 @@ def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
 
-    if os.envron.get('DATABASE_URL', ''):
+    if os.environ.get('DATABASE_URL', ''):
         settings['sqlalchemy.url'] = os.environ['DATABASE_URL']
 
     config = Configurator(settings=settings)
